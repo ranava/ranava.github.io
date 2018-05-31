7bc1e79535ec3c781203f42daab160133cf09701
Ry <mitchellr86@gmail.com>
2018-05-31 15:10:13 -0500

    Add files via upload
    Updated to include 2019, 2020, and the new rotating schedule

--- schedule/calendar.js
+++ schedule/calendar.js
@@ -1,3 +1,33 @@
 window.onload = function () {
-  $("#accordion").accordion();
+  $("#accordion18").accordion();
+  $("#accordion19").accordion();
+  $("#accordion20").accordion();
+  $('.calendars').hide();
+  $('.2018').show();
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+ window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function yearChange(year) {
+  $('.calendars').hide();
+  $('.' + year).show();
 }
