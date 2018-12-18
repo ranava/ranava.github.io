49ffbd83bcb38bcb105ad90b19bfdac49df9c82f
Ry <mitchellr86@gmail.com>
2018-12-18 14:32:39 -0600

    Create booklevels.js

---
+++ authorlevels/booklevels.js
@@ -0,0 +1,112 @@
+var bookone = "booktemplate.png";
+var booktwo = "booktemplate.png";
+var bookthree = "booktemplate.png";
+var bookfour = "booktemplate.png";
+var bookfive = "booktemplate.png";
+var booksix = "booktemplate.png";
+var bookseven = "booktemplate.png";
+var bookeight = "booktemplate.png";
+
+function changeImageOne() {
+  if ( bookone == "booktemplate.png" ) {
+    document.images["bookone"].src = "booktemplateblack.png";
+    document.images["bookone"].alt = "First Step Complete";
+    bookone  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookone"].src = "booktemplate.png";
+    document.images["bookone"].alt = "First Step Incomplete";
+    bookone  = "booktemplate.png";
+  }
+}
+
+function changeImageTwo() {
+  if ( booktwo == "booktemplate.png" ) {
+    document.images["booktwo"].src = "booktemplateblack.png";
+    document.images["booktwo"].alt = "Second Step Complete";
+    booktwo  = "booktemplateblack.png";
+  }
+  else {
+    document.images["booktwo"].src = "booktemplate.png";
+    document.images["booktwo"].alt = "Second Step Incomplete";
+    booktwo  = "booktemplate.png";
+  }
+}
+
+function changeImageThree() {
+  if ( bookthree == "booktemplate.png" ) {
+    document.images["bookthree"].src = "booktemplateblack.png";
+    document.images["bookthree"].alt = "Third Step Complete";
+    bookthree  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookthree"].src = "booktemplate.png";
+    document.images["bookthree"].alt = "Third Step Incomplete";
+    bookthree  = "booktemplate.png";
+  }
+}
+
+function changeImageFour() {
+  if ( bookfour == "booktemplate.png" ) {
+    document.images["bookfour"].src = "booktemplateblack.png";
+    document.images["bookfour"].alt = "Fourth Step Complete";
+    bookfour  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookfour"].src = "booktemplate.png";
+    document.images["bookfour"].alt = "Fourth Step Incomplete";
+    bookfour  = "booktemplate.png";
+  }
+}
+
+function changeImageFive() {
+  if ( bookfive == "booktemplate.png" ) {
+    document.images["bookfive"].src = "booktemplateblack.png";
+    document.images["bookfive"].alt = "Fifth Step Complete";
+    bookfive  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookfive"].src = "booktemplate.png";
+    document.images["bookfive"].alt = "Fifth Step Incomplete";
+    bookfive  = "booktemplate.png";
+  }
+}
+
+function changeImageSix() {
+  if ( booksix == "booktemplate.png" ) {
+    document.images["booksix"].src = "booktemplateblack.png";
+    document.images["booksix"].alt = "Sixth Step Complete";
+    booksix  = "booktemplateblack.png";
+  }
+  else {
+    document.images["booksix"].src = "booktemplate.png";
+    document.images["booksix"].alt = "Sixth Step Incomplete";
+    booksix  = "booktemplate.png";
+  }
+}
+
+function changeImageSeven() {
+  if ( bookseven == "booktemplate.png" ) {
+    document.images["bookseven"].src = "booktemplateblack.png";
+    document.images["bookseven"].alt = "Seventh Step Complete";
+    bookseven  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookseven"].src = "booktemplate.png";
+    document.images["bookseven"].alt = "Seventh Step Incomplete";
+    bookseven  = "booktemplate.png";
+  }
+}
+
+function changeImageEight() {
+  if ( bookeight == "booktemplate.png" ) {
+    document.images["bookeight"].src = "booktemplateblack.png";
+    document.images["bookeight"].alt = "Eighth Step Complete";
+    bookeight  = "booktemplateblack.png";
+  }
+  else {
+    document.images["bookeight"].src = "booktemplate.png";
+    document.images["bookeight"].alt = "Eighth Step Incomplete";
+    bookeight  = "booktemplate.png";
+  }
+}
