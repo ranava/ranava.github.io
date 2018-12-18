56771a53e252d6b94b084689fc8aeae14224c35a
Ry <mitchellr86@gmail.com>
2018-12-18 14:32:56 -0600

    Create booklevelsshields.js

---
+++ authorlevels/booklevelsshields.js
@@ -0,0 +1,252 @@
+var shieldone = "shieldtemplate.png"
+var shieldtwo = "shieldtemplate.png"
+var shieldthree = "shieldtemplate.png"
+var shieldfour = "shieldtemplate.png"
+var shieldfive = "shieldtemplate.png"
+var shieldsix = "shieldtemplate.png"
+var shieldseven = "shieldtemplate.png"
+var shieldeight = "shieldtemplate.png"
+var shieldnine = "shieldtemplate.png"
+var shieldten = "shieldtemplate.png"
+var shieldeleven = "shieldtemplate.png"
+var shieldtwelve = "shieldtemplate.png"
+
+function changeShieldOne() {
+  if ( shieldone == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "shieldtemplate.png";
+    document.images["shieldthree"].src = "shieldtemplate.png";
+    document.images["shieldfour"].src = "shieldtemplate.png";
+    document.images["shieldfive"].src = "shieldtemplate.png";
+    document.images["shieldsix"].src = "shieldtemplate.png";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Plankton!";
+  }
+}
+
+function changeShieldTwo() {
+  if ( shieldtwo == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "shieldtemplate.png";
+    document.images["shieldfour"].src = "shieldtemplate.png";
+    document.images["shieldfive"].src = "shieldtemplate.png";
+    document.images["shieldsix"].src = "shieldtemplate.png";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Prawn!";
+  }
+}
+
+function changeShieldThree() {
+  if ( shieldthree == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].alt = "Plankton Level!";
+    document.images["shieldfour"].src = "shieldtemplate.png";
+    document.images["shieldfive"].src = "shieldtemplate.png";
+    document.images["shieldsix"].src = "shieldtemplate.png";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Lobster!";
+  }
+}
+
+function changeShieldFour() {
+  if ( shieldfour == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].alt = "Plankton Level!";
+    document.images["shieldfive"].src = "shieldtemplate.png";
+    document.images["shieldsix"].src = "shieldtemplate.png";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Trout!";
+  }
+}
+
+function changeShieldFive() {
+  if ( shieldfive == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].alt = "Plankton Level!";
+    document.images["shieldsix"].src = "shieldtemplate.png";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Rainbow Trout!";
+  }
+}
+
+function changeShieldSix() {
+  if ( shieldsix == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].alt = "Plankton Level!";
+    document.images["shieldseven"].src = "shieldtemplate.png";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Salmon!";
+  }
+}
+
+function changeShieldSeven() {
+  if ( shieldseven == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].alt = "Plankton Level!";
+    document.images["shieldeight"].src = "shieldtemplate.png";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Tuna!";
+  }
+}
+
+function changeShieldEight() {
+  if ( shieldeight == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].alt = "Plankton Level!";
+    document.images["shieldnine"].src = "shieldtemplate.png";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Dolphin!";
+  }
+}
+
+function changeShieldNine() {
+  if ( shieldnine == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].src = "brightshieldtemplate.png";
+    document.images["shieldnine"].src = "brightshieldtemplate.png";
+    document.images["shieldnine"].alt = "Plankton Level!";
+    document.images["shieldten"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Giant Squid!";
+  }
+}
+
+function changeShieldTen() {
+  if ( shieldten == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].src = "brightshieldtemplate.png";
+    document.images["shieldnine"].src = "brightshieldtemplate.png";
+    document.images["shieldten"].src = "brightshieldtemplate.png";
+    document.images["shieldten"].alt = "Plankton Level!";
+    document.images["shieldeleven"].src = "shieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Whale!";
+  }
+}
+
+function changeShieldEleven() {
+  if ( shieldeleven == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].src = "brightshieldtemplate.png";
+    document.images["shieldnine"].src = "brightshieldtemplate.png";
+    document.images["shieldten"].src = "brightshieldtemplate.png";
+    document.images["shieldeleven"].src = "brightshieldtemplate.png";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+    document.images["shieldeleven"].alt = "Plankton Level!";
+    document.images["shieldtwelve"].src = "shieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Kelpie!";
+  }
+}
+
+function changeShieldTwelve() {
+  if ( shieldtwelve == "shieldtemplate.png" ) {
+    document.images["shieldone"].src = "brightshieldtemplate.png";
+    document.images["shieldtwo"].src = "brightshieldtemplate.png";
+    document.images["shieldthree"].src = "brightshieldtemplate.png";
+    document.images["shieldfour"].src = "brightshieldtemplate.png";
+    document.images["shieldfive"].src = "brightshieldtemplate.png";
+    document.images["shieldsix"].src = "brightshieldtemplate.png";
+    document.images["shieldseven"].src = "brightshieldtemplate.png";
+    document.images["shieldeight"].src = "brightshieldtemplate.png";
+    document.images["shieldnine"].src = "brightshieldtemplate.png";
+    document.images["shieldten"].src = "brightshieldtemplate.png";
+    document.images["shieldeleven"].src = "brightshieldtemplate.png";
+    document.images["shieldtwelve"].src = "brightshieldtemplate.png";
+    document.images["shieldtwelve"].alt = "Plankton Level!";
+    shieldtwelve  = "brightshieldtemplate.png";
+
+    document.getElementById("leveltitle").innerHTML = "a Kraken!";
+  }
+}
