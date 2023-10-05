4f8e7b919a0fac14234065f6bdc1fd5d992151dd
Ry <mitchellr86@gmail.com>
2023-10-05 14:10:04 -0500

    Add files via upload
    Adding 2024 calendars

---
+++ schedule/calendarn.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  $("#accordion23").accordion();
+  $("#accordion24").accordion();
+  $('.calendars').hide();
+  $('.2024').show();
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("Dropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+ window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function yearChange(year) {
+  $('.calendars').hide();
+  $('.' + year).show();
+}
